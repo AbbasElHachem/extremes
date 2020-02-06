b6afb1baaf0ab4ac4638e5aa538e82d0d309ac71
hachem <hachem@lhg-102.iws.uni-stuttgart.de>
2020-02-06 16:26:11 +0100

    plot

--- _57_plot_radar_events_BW_.py
+++ _57_plot_radar_events_BW_.py
@@ -107,16 +107,11 @@ for i, file in enumerate(files):
         mask = np.ones_like(lon1, dtype=np.bool)
 
         for n, i_poly in enumerate(first['geometry']['coordinates']):
-            # print(n)
-            #         plt.plot(np.array(i_poly)[0, :, 0],
-            #                  np.array(i_poly)[0, :, 1],
-            #                  linestyle='-',
-            #                  linewidth=0.8,
-            #                  color='black'
-            #                  )
+
             p = path.Path(np.array(i_poly)[0, :, :])
             grid_mask = p.contains_points(
-                np.vstack((lon1.flatten(), lat1.flatten())).T).reshape(900, 900)
+                np.vstack((lon1.flatten(),
+                           lat1.flatten())).T).reshape(900, 900)
             mask[grid_mask] = 0
 
         rwdata[mask] = -1
@@ -140,17 +135,12 @@ for i, file in enumerate(files):
         # plt.xlim([netatmo.get_xlim()[0], netatmo.get_xlim()[1]])
         # plt.ylim([netatmo.get_ylim()[0], netatmo.get_ylim()[1]])
         cbar = plt.colorbar(extend='max', label='[mm/h]')
-        # cbar.set_label('Hourly Precipitation [mm]', rotation=270)
-        # colorbar.colorbar(ax0)
-    #     plt.axis('equal')
-    #     plt.scatter(dwd_lons.values, dwd_lats.values,
-    #                 marker='x', alpha=0.5, color='k', s=10)
-    #     plt.scatter(grid_lons, grid_lats, marker='.', color='r', alpha=0.5, s=10)
 
         plt.tight_layout()
         plt.savefig(
-            Path(os.path.join(r'C:\Users\hachem\Desktop\radar',
-                              'hourly_event_{}.png'.format(file[50:59]))), dpi=600)
+            Path(os.path.join(
+                r'C:\Users\hachem\Desktop\radar',
+                'hourly_event_{}.png'.format(file[50:59]))), dpi=600)
 
     #     plt.show()
     #     break
